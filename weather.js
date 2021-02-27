59fd7f6f8f671f904ecc3bfcca3e4d97e202877d
Park Byeong Jin <67573062+fatejin@users.noreply.github.com>
2021-02-27 17:39:58 +0900

    #2-2 Getting the Weather part Two API (FInish)

--- weather.js
+++ weather.js
@@ -1,5 +1,18 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "a409e9300791ac193b803e099caeef8b";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });  //javascript fetch, difficult one
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +26,7 @@ function handleGeoSucess(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -24,11 +38,13 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadedCords = localStorage.getItem(COORDS);
-    if (loadedCords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
         askForCoords();
     } else {
-        // getweather
+        const parseCoords = JSON.parse(loadedCoords);
+        console.log(parseCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
